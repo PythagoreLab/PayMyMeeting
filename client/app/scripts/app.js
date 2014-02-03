495f33b82bcb0947ea6a58e12854c20c508c0f49
Vincent Bostoen <vincent.bostoen@gmail.com>
2014-02-03 17:44:46 +0100

    Add very basic creation and meeting views

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -12,6 +12,14 @@ angular.module('payMyMeetingApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/create', {
+        templateUrl: 'views/create.html',
+        controller: 'MainCtrl'
+      })
+      .when('/meeting', {
+        templateUrl: 'views/meeting.html',
+        controller: 'MainCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
