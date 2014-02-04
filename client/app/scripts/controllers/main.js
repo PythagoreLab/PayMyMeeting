cec1c4a923e2e8fd0959048f30645e6fd3def5bd
Vincent Bostoen <vincent.bostoen@gmail.com>
2014-02-04 16:01:00 +0100

    Query REST api from controller and add ng-click on some buttons

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,11 +1,31 @@
 'use strict';
 
 angular.module('payMyMeetingApp')
-.controller('homeController', function ($scope) {
+.controller('homeController', function ($scope, $http) {
+	$http.get('/api/meetings').
+	success(function (data, status, headers, config) {
+		$scope.meetings = data;
+	}).
+	error(function (data, status, headers, config) {
+	});
 })
-.controller('createMeetingController', function ($scope) {
+.controller('createMeetingController', function ($scope, $http) {
+	$scope.createMeeting = function(meeting){
+		$http.post('/api/meetings/', meeting).
+		success(function (data, status, headers, config) {
+		}).
+		error(function (data, status, headers, config) {
+			$window.alert("OOPS ! An error occured while creating the meeting.");
+		});
+	}
 })
-.controller('showMeetingController', function ($scope) {
+.controller('showMeetingController', function ($scope, $http) {
+	$http.get('/api/attendees/52f0f5e3404452581ab77e05').
+	success(function (data, status, headers, config) {
+		$scope.attendees = data;
+	}).
+	error(function (data, status, headers, config) {
+	});
 	var sock = new SockJS('http://127.0.0.1:3000/notifications');
 
 	sock.onopen = function() {
@@ -20,7 +40,14 @@ angular.module('payMyMeetingApp')
 		console.log('socket connection closed');
 	};
 })
-.controller('joinMeetingController', function ($scope) {
+.controller('joinMeetingController', function ($scope, $http) {
+	$scope.joinMeeting = function(joinMeetingRequest){
+		$http.post('/api/attendees/', joinMeetingRequest).
+		success(function (data, status, headers, config) {
+		}).
+		error(function (data, status, headers, config) {
+		});
+	}
 })
 .controller('countMeetingCostController', function ($scope) {
 });
