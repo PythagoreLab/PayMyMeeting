c4a4ad1389fab75341675957bc9c1d948410feb5
Jeremy Alles <jeremy.alles@gmail.com>
2014-02-05 16:34:23 +0100

    Refresh attendees list when a new attendee join the meeting

--- client/app/scripts/controllers/showMeetingController.js
+++ client/app/scripts/controllers/showMeetingController.js
@@ -4,20 +4,25 @@ angular.module('payMyMeetingApp')
 .controller('showMeetingController', function ($scope, $http, $routeParams, $log) {
 	$scope.meetingId = $routeParams.meetingId;
 
-	$http.get('/api/attendees/' + $scope.meetingId).
-	success(function (data) {
-		$scope.attendees = data;
-	}).
-	error(function () {
-	});
+	var loadAttendees = function(){
+		$http.get('/api/attendees/' + $scope.meetingId).
+		success(function (data) {
+			$scope.attendees = data;
+		}).
+		error(function () {
+		});
+	};
+
+	loadAttendees();
+
 	var sock = new SockJS('http://127.0.0.1:3000/notifications');
 
 	sock.onopen = function() {
 		$log.log('socket connection open');
 	};
 
-	sock.onmessage = function(e) {
-		$log.log('socket message ' + e.data);
+	sock.onmessage = function() {
+		loadAttendees();
 	};
 
 	sock.onclose = function() {
