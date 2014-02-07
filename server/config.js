1e3a777d15a8cb94e9d8a162b2010779068151c4
Vincent Bostoen <vincent.bostoen@gmail.com>
2014-02-07 16:56:53 +0100

    Remove mongolab apikey

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ path = require('path');
 module.exports = {
     mongo: {
     dbUrl: 'https://api.mongolab.com/api/1',            // The base url of the MongoLab DB server
-    apiKey: 'gubV4zs5yN-ItUUQDpgUx0pUA98K3MYH',         // Our MongoLab API key
+    apiKey: '',         // Our MongoLab API key
     dbPath: 'mongodb://localhost:27017'
   },
   server: {
